1bce4c74499266e09186d55fb96a113e726b14a5
Márk Kaposvári <kaposvari.markz@gmail.com>
2020-10-02 20:39:53 +0200

    Added a class, a form and the resources.

---
+++ UserMaintenance/UserMaintenance/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserMaintenance.Entities
+{
+    class User
+    {
+        public Guid ID { get; set; } = Guid.NewGuid();
+        public String Firstname { get; set; }
+        public String LastName { get; set; }
+        public String FullName
+        {
+            get
+            {
+                return string.Format("{0} {1}",LastName,Firstname);
+            }
+        
+        }
+
+    }
+}

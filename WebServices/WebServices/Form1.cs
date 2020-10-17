7aa8b0960f6e0b6bd9738af76d1d549c7a71fcc2
Márk Kaposvári <kaposvari.markz@gmail.com>
2020-10-17 22:58:35 +0200

    VI02-3 - added webservice and created call webservice logic

--- WebServices/WebServices/Form1.cs
+++ WebServices/WebServices/Form1.cs
@@ -7,14 +7,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WebServices.MnbServiceReference;
 
 namespace WebServices
 {
     public partial class Form1 : Form
     {
+
+
         public Form1()
         {
             InitializeComponent();
+            GetExchangeRates();
+        }
+
+        private void GetExchangeRates()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
+            richTextBox1.Text = result;
         }
     }
 }

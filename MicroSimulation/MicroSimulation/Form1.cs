14c459d443d2ea3f0cc7912d0af4e5a4397b90aa
Márk Kaposvári <kaposvari.markz@gmail.com>
2020-10-25 22:49:46 +0100

    VII06 - added 'for' loop for simulation

--- MicroSimulation/MicroSimulation/Form1.cs
+++ MicroSimulation/MicroSimulation/Form1.cs
@@ -28,9 +28,22 @@ namespace MicroSimulation
             BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
             DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
 
-            dataGridView1.DataSource = Population;
-            dataGridView2.DataSource = BirthProbabilities;
-            dataGridView3.DataSource = DeathProbabilities;
+            for (int year = 2005; year <= 2024; year++)
+            {
+                for (int i = 0; i < Population.Count; i++)
+                {
+
+                }
+
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                Console.WriteLine(
+                    string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+            }
         }
 
         public List<Person> GetPopulation(string csvpath)

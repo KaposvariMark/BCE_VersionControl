b25edebfd26bef6d5b5359f8ced2fde781d5d876
Márk Kaposvári <kaposvari.markz@gmail.com>
2020-10-25 23:37:21 +0100

    VII08 - added lists to count pops by gender

--- MicroSimulation/MicroSimulation/Form1.cs
+++ MicroSimulation/MicroSimulation/Form1.cs
@@ -17,6 +17,8 @@ namespace MicroSimulation
         List<Person> Population = new List<Person>();
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
+        List<int> MalePop = new List<int>();
+        List<int> FemalePop = new List<int>();
         Random rng = new Random(1234);
 
         public Form1()
@@ -142,6 +144,9 @@ namespace MicroSimulation
                                     select x).Count();
                 Console.WriteLine(
                     string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+
+                MalePop.Add(nbrOfMales);
+                FemalePop.Add(nbrOfFemales);
             }
         }
 

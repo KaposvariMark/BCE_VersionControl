3face0d5987bc8f02ff96ab91c99f527157835dd
Márk Kaposvári <kaposvari.markz@gmail.com>
2020-11-27 15:27:10 +0100

    X04 - created starting population

--- EvolutionaryAlgorithm/EvolutionaryAlgorithm/Form1.Designer.cs
+++ EvolutionaryAlgorithm/EvolutionaryAlgorithm/Form1.Designer.cs
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbl_gen = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_gen
+            // 
+            this.lbl_gen.AutoSize = true;
+            this.lbl_gen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbl_gen.Location = new System.Drawing.Point(638, 20);
+            this.lbl_gen.Name = "lbl_gen";
+            this.lbl_gen.Size = new System.Drawing.Size(96, 20);
+            this.lbl_gen.TabIndex = 0;
+            this.lbl_gen.Text = "1. generáció";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_gen);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbl_gen;
     }
 }
 
--- EvolutionaryAlgorithm/EvolutionaryAlgorithm/Form1.cs
+++ EvolutionaryAlgorithm/EvolutionaryAlgorithm/Form1.cs
@@ -13,16 +13,29 @@ namespace EvolutionaryAlgorithm
 {
     public partial class Form1 : Form
     {
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+
         GameController gc = new GameController();
         GameArea ga;
+
         public Form1()
         {
             InitializeComponent();
 
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
-            gc.AddPlayer();
-            gc.Start(true);
+            //gc.AddPlayer();
+            //gc.Start(true);
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+            gc.Start();
         }
+
     }
 }
